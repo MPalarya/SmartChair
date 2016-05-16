d2e350a436067c998f7fc0d88e42190a6cb4294d
Michael Palarya <palarya@gmail.com>
2016-05-16 13:49:19 +0300

    initial layout

---
+++ RPi/RPi Server API/Alert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPi.RPi_Server_API
+{
+    enum EAlertMsg
+    {
+        [Description("Success")]
+        Success,
+
+        [Description("Fail")]
+        Fail,
+
+        // TODO: define
+    }
+
+    class CAlert
+    {
+        #region Fields
+
+        private UInt32 _deviceId;
+        private EAlertMsg _message;
+
+        #endregion
+
+        #region Contructors
+
+
+        #endregion
+
+        #region Properties
+
+        public UInt32 DeviceID
+        {
+            get { return _deviceId; }
+        }
+
+        public EAlertMsg Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+
+    }
+}
