5a34ceb169c1c85a0fbb7075986768becfd20b2e
Michael Palarya <palarya@gmail.com>
2016-05-17 17:53:22 +0300

    currently not returning from await

--- RPi/MainPage.xaml.cs
+++ RPi/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using RPi.RPi_Hardware;
@@ -10,26 +11,59 @@ namespace RPi
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        CSensor bigSensor1 = new CSensor(ESensorType.FlexiForceA201, 0);
+        CSensor bigSensor2 = new CSensor(ESensorType.FlexiForceA201, 1);
+
+        CSensor smallSensor1 = new CSensor(ESensorType.SquareForceResistor, 2);
+        CSensor smallSensor2 = new CSensor(ESensorType.SquareForceResistor, 3);
+
         public MainPage()
         {
             // initial setup
             CChair myChair = new CChair();
 
-            CSensor bigSensor1 = new CSensor(ESensorType.FlexiForceA201, 0);
-            CSensor bigSensor2 = new CSensor(ESensorType.FlexiForceA201, 1);
 
-            CSensor smallSensor1 = new CSensor(ESensorType.SquareForceResistor, 2);
-            CSensor smallSensor2 = new CSensor(ESensorType.SquareForceResistor, 3);
 
             myChair.Seat.Add(EChairPartArea.LeftMid, bigSensor1);
             myChair.Seat.Add(EChairPartArea.RightMid, bigSensor2);
 
             myChair.Back.Add(EChairPartArea.LeftMid, smallSensor1);
             myChair.Back.Add(EChairPartArea.RightMid, smallSensor2);
-
+            
 
             this.InitializeComponent();
         }
 
+        private void buttonRead1_Click(object sender, RoutedEventArgs e)
+        {
+            double w = Math.Round(double.Parse(textBoxWeight1.Text), 2);
+            double v = Math.Round(bigSensor1.Read(), 2);
+            double c = Math.Round(w/v, 2);
+            textRead1.Text = "- W:" + w + " mV: " + v + " C: " + c;
+        }
+
+        private void buttonRead2_Click(object sender, RoutedEventArgs e)
+        {
+            double w = Math.Round(double.Parse(textBoxWeight2.Text), 2);
+            double v = Math.Round(bigSensor1.Read(), 2);
+            double c = Math.Round(w / v, 2);
+            textRead2.Text = "- W:" + w + " mV: " + v + " C: " + c;
+        }
+
+        private void buttonRead3_Click(object sender, RoutedEventArgs e)
+        {
+            double w = Math.Round(double.Parse(textBoxWeight3.Text), 2);
+            double v = Math.Round(bigSensor1.Read(), 2);
+            double c = Math.Round(w / v, 2);
+            textRead3.Text = "- W:" + w + " mV: " + v + " C: " + c;
+        }
+
+        private void buttonRead4_Click(object sender, RoutedEventArgs e)
+        {
+            double w = Math.Round(double.Parse(textBoxWeight4.Text), 2);
+            double v = Math.Round(bigSensor1.Read(), 2);
+            double c = Math.Round(w / v, 2);
+            textRead4.Text = "- W:" + w + " mV: " + v + " C: " + c;
+        }
     }
 }
