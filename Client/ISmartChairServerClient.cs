6e1d18793435486c9bb0eb3d8cc0597404d9148c
sivanhizkiev <sivan.hiz@gmail.com>
2016-05-28 15:21:13 +0300

    Adding routing and server interface

---
+++ Client/ISmartChairServerClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public interface ISmartChairServerClient
+    {
+        /// <summary>
+        /// Sends the email to server. If email does not exist in the server- add it
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>returns true if this user have logged in before, otherwise false</returns>
+        bool login(string email);
+
+        /// <summary>
+        /// Calibarates smartchair according to the person. Occurs only on the first time, when we
+        /// don't have the user's email in the DB
+        /// </summary>
+        /// <param name="email">to link data to user</param>
+        /// <returns>returns true if initialization succeeded, otherwise false</returns>
+        bool initialize(string email);
+
+        /// <summary>
+        /// Gets the data saved from the last week. 3 times for every day 11AM, 14PM, 18PM
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>A list of the the int value that was measured and the time it was measured in</returns>
+        Dictionary<DateTime, int> getLastWeekData(string email);
+
+
+
+    }
+}
