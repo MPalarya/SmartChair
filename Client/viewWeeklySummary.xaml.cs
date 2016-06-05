e1a94b445e750513afce9ca915b7f5a36c063aa4
sivanhizkiev <sivan.hiz@gmail.com>
2016-06-05 20:46:34 +0300

    Adding charts to weekly summary

--- Client/viewWeeklySummary.xaml.cs
+++ Client/viewWeeklySummary.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,6 +27,19 @@ namespace Client
         {
             this.DataContext = new weeklySummaryController();
             this.InitializeComponent();
+            this.Loaded += ViewWeeklySummary_Loaded;
+        }
+
+        private void ViewWeeklySummary_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<alertData> data = new List<alertData>();
+
+            data.Add(new alertData() { time = new DateTime(2016, 6, 5, 10, 15, 0), alertScale = 1});
+            data.Add(new alertData() { time = new DateTime(2016, 6, 2, 14, 23, 0) ,alertScale = 5});
+            data.Add(new alertData() { time = new DateTime(2016, 6, 1, 14, 20, 0) ,alertScale = 22 });
+
+            (weeklySummary.Series[0] as LineSeries).ItemsSource = data;
+            
         }
     }
 }
