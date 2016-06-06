c1b64afcb0c5d59a02c83c5d16463665fa2aab74
orrshilon <orr@shilons.co.il>
2016-06-06 09:10:30 +0300

    try to simulate

---
+++ RPiSimulator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace RPiSimulator
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "smartchair-iothub.azure-devices.net";
+        static string deviceKey;
+
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "..\\..\\..\\GetDeviceIdentity\\bin\\Release\\GetDeviceIdentity.exe";
+            p.Start();
+
+            deviceKey = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine("DeviceKey: {0}\n", deviceKey);
+
+            Console.WriteLine("Simulated RPi\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("00261-30000-00000-AA825", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myFirstDevice",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
